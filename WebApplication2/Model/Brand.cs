3d746966ab05776bc80b05c701d931db2550874e
tamanchichan <tamanchichan@gmail.com>
2023-08-03 10:25:59 -0500

    code refactored

---
+++ WebApplication2/Model/Brand.cs
@@ -0,0 +1,30 @@
+﻿namespace LaptopStoreRefactor.Model
+{
+  public class Brand
+  {
+    public int Id { get; set; }
+        
+    public string _name;
+    public string Name
+    {
+      get
+      {
+        return _name;
+      }
+      set
+      {
+        if (string.IsNullOrEmpty(value) || value.Length < 3 )
+        {
+          throw new ArgumentOutOfRangeException(
+            nameof(value),
+            "Brand name must be at least three characters in length."
+            );
+        }
+
+        _name = value;
+      }
+    }
+
+    public HashSet<Laptop> Laptops = new HashSet<Laptop>();
+  }
+}
---
+++ WebApplication2/Model/Laptop.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace LaptopStoreRefactor.Model
+{
+  public class Laptop
+  {
+    public Guid Number { get; set; }
+
+    private string _model;
+    public string Model
+    {
+      get
+      {
+        return _model;
+      }
+      set
+      {
+        if (string.IsNullOrEmpty(value) || value.Length < 3)
+        {
+          throw new ArgumentOutOfRangeException(
+            nameof(value),
+            "Laptop model name must be at least three characters in length."
+            );
+        }
+      }
+    }
+
+    private decimal _price;
+    public decimal Price
+    {
+      get
+      {
+        return _price;
+      }
+      set
+      {
+        if (value < 0)
+        {
+          throw new ArgumentOutOfRangeException("Price cannot be less than zero.");
+        }
+
+        _price = value;
+      }
+    }
+
+    public LaptopCondition Condition { get; set; }
+
+    public int BrandId { get; set; }
+    public Brand Brand { get; set; }
+  }
+
+  public enum LaptopCondition
+  {
+    New,
+    Refurbished,
+    Rental
+  }
+}
