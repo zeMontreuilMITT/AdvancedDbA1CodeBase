5b77374d15f4954ce829f27f07854c89aee640ab
Noah <noseby@live.ca>
2023-08-04 22:49:10 -0500

    adding code

---
+++ WebApplication2/Models/Store.cs
@@ -0,0 +1,48 @@
+﻿namespace WebApplication2.Models
+{
+    public class Store
+    {
+
+        public Guid StoreId { get; set; }
+
+        public string StreetNameAndNumber { get; set; }
+
+        public string Province { get; set; }
+
+        public Laptop Laptops { get; set; }
+
+        private void SetProvince(string province)
+        {
+            // set string array to restrict provinces
+            string[] canadaProvinces = 
+                { 
+                "Alberta",
+                "British Columbia",
+                "Manitoba",
+                "New Brunswick",
+                "Newfoundland and Labrador",
+                "Nova Scotia",
+                "Ontario",
+                "Prince Edward Island",
+                "Quebec",
+                "Saskatchewan"
+            };
+            //https://stackoverflow.com/questions/72696/which-is-generally-best-to-use-stringcomparison-ordinalignorecase-or-stringcom
+
+            if (!canadaProvinces.Contains(province, StringComparer.OrdinalIgnoreCase))
+            {
+                throw new ArgumentException("Invalid province. Please choose a valid Canadian province.", nameof(province));
+            }
+
+            Province = province;
+        }
+
+        public Store(Guid storeId, string streetNameAndNumber, string province)
+        {
+            StoreId = storeId;
+            StreetNameAndNumber = streetNameAndNumber;
+            SetProvince(province);
+        }
+
+    }
+}
--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using WebApplication2.Models;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = builder.Configuration.GetConnectionString("SlnDbContext");
+
+builder.Services.AddDbContext<SlnDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
